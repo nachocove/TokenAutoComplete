35c3088e306e185036721c6ab4ba7f1e363b4a67
Owen Shaw <owen@owenshaw.com>
2017-04-29 11:32:42 -0700

    adjust token view to use text baseline if available, and assume no font metrics adjustments are needed
    goal is to have tokens inserted aligned with text and without causing the line to change size, which
    creates a wobble when typing an inserting tokens

--- library/src/main/java/com/tokenautocomplete/ViewSpan.java
+++ library/src/main/java/com/tokenautocomplete/ViewSpan.java
@@ -5,6 +5,7 @@ import android.graphics.Paint;
 import android.text.style.ReplacementSpan;
 import android.view.View;
 import android.view.ViewGroup;
+import android.util.Log;
 
 /**
  * Span that holds a view it draws when rendering
@@ -31,13 +32,19 @@ public class ViewSpan extends ReplacementSpan {
         view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
     }
 
-    public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, Paint paint) {
+    public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int baseline, int bottom, Paint paint) {
         prepView();
 
         canvas.save();
-        //Centering the token looks like a better strategy that aligning the bottom
-        int padding = (bottom - top - view.getBottom()) / 2;
-        canvas.translate(x, bottom - view.getBottom() - padding);
+        int viewBaseline = view.getBaseline();
+        // Log.i("TOKENAUTOCOMPLETE", String.format("draw     0x%08x at %d,%d; base=%d; viewBase=%d; v %dx%d", System.identityHashCode(view), top, bottom, baseline, viewBaseline, view.getMeasuredWidth(), view.getMeasuredHeight()));
+        if (viewBaseline == -1){
+            //Centering the token looks like a better strategy that aligning the bottom
+            int padding = (bottom - top - view.getBottom()) / 2;
+            canvas.translate(x, bottom - view.getBottom() - padding);
+        }else{
+            canvas.translate(x, baseline - viewBaseline);
+        }
         view.draw(canvas);
         canvas.restore();
     }
@@ -46,17 +53,18 @@ public class ViewSpan extends ReplacementSpan {
         prepView();
 
         if (fm != null) {
+            // Log.i("TOKENAUTOCOMPLETE", String.format("getSize  0x%08x with a=%d, d=%d, t=%d, b=%d, l=%d, v=%d", System.identityHashCode(view), fm.ascent, fm.descent, fm.top, fm.bottom, fm.leading, view.getMeasuredHeight()));
             //We need to make sure the layout allots enough space for the view
             int height = view.getMeasuredHeight();
             int need = height - (fm.descent - fm.ascent);
-            if (need > 0) {
-                int ascent = need / 2;
-                //This makes sure the text drawing area will be tall enough for the view
-                fm.descent += need - ascent;
-                fm.ascent -= ascent;
-                fm.bottom += need - ascent;
-                fm.top -= need / 2;
-            }
+            // if (need > 0) {
+            //     int ascent = need / 2;
+            //     //This makes sure the text drawing area will be tall enough for the view
+            //     fm.descent += need - ascent;
+            //     fm.ascent -= ascent;
+            //     fm.bottom += need - ascent;
+            //     fm.top -= need / 2;
+            // }
         }
 
         return view.getRight();
